e23e3311e49c65b56e7f3da531e6820e1cbbc8cb
shoishizaki <shoishizaki1997@gmail.com>
2019-07-18 11:40:35 +0900

    Change phone model

--- employee_management/model/employee_model.py
+++ employee_management/model/employee_model.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Employee(models.Model):
     name = models.CharField(max_length=200)
-    phone = models.CharField(max_length=200)
+    phone = models.BigIntegerField()
     home = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
     
