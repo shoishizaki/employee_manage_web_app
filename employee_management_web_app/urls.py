a6db2c9dd8b2dd58c0179322996d1d0e58e6f098
shoishizaki <shoishizaki1997@gmail.com>
2019-07-18 11:48:17 +0900

    Change url path

--- employee_management_web_app/urls.py
+++ employee_management_web_app/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.urls import include
 
 urlpatterns = [
-    path('employee_management/', include('employee_management.urls')),
+    path('', include('employee_management.urls')),
     path('admin/', admin.site.urls),
 ]
