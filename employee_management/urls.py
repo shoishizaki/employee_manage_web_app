dc7cfa0f4389b4ca959d730adcead4bd8cd5ba81
shoishizaki <shoishizaki1997@gmail.com>
2019-07-26 22:25:36 +0900

    Add add_function url

--- employee_management/urls.py
+++ employee_management/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.first_page, name='first_page'),
     path('detail/', views.detail_page, name='detail_page'),
+    path('add/', views.add_function, name='add'),
 ]
