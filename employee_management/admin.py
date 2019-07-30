ede5de5f95309a39d340e31cbcfe766354886a58
shoishizaki <shoishizaki1997@gmail.com>
2019-07-30 11:05:46 +0900

    Create employee model

--- employee_management/admin.py
+++ employee_management/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from employee_management.model.employee_model import Employee
 
 # Register your models here.
+
+admin.site.register(Employee)
--- employee_management/model/employee_model.py
+++ employee_management/model/employee_model.py
@@ -1,8 +1,11 @@
 from django.db import models
 
+
 class Employee(models.Model):
     name = models.CharField(max_length=200)
     phone = models.BigIntegerField()
     home = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
-    
+
+    def __str__(self):
+        return self.name
